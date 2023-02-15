8b81ce5011b007ea3604d3011b1cdebd45bc1c3e
hopefullyicanchangethislater <121258893+hopefullyicanchangethislater@users.noreply.github.com>
2023-02-14 20:50:05 -0500

    Add project files.

---
+++ Stupid Temperature Converter/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// This is a dumb temperature converter. You should make yourself a smart one.
+int fahrenheit = 94;
+int mid = fahrenheit - 32;
+
+int celsuis = (fahrenheit - 32) * 5 / 9;
+Console.WriteLine($@"The temperature is {celsuis} Celsuis.");
